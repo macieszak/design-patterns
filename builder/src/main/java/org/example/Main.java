c04c8ec40389023a50d2c7de00a6c375196db962
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-14 19:48:51 +0200

    Initial status of the Builder pattern.

---
+++ builder/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.house.House;
+
+public class Main {
+    public static void main(String[] args) {
+
+        House house1 = new House("walls", "floors", "rooms", "windows", "doors", "garage");
+
+    }
+}
