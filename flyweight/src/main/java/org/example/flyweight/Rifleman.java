5385daa2fc72347ea04a4ba0c7825a73e8382622
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-15 19:08:26 +0200

    Initial status before implementing Flyweight pattern.

---
+++ flyweight/src/main/java/org/example/flyweight/Rifleman.java
@@ -0,0 +1,27 @@
+package org.example.flyweight;
+
+public class Rifleman {
+
+    private String name;
+    private int hp;
+    private int armour;
+    private int damageDealt;
+    private int speed;
+    private int x;
+    private int y;
+    private int hpLeft;
+    private int resourceCost;
+
+
+    public Rifleman(String name, int hp, int armour, int damageDealt, int speed, int x, int y, int resourceCost) {
+        this.name = name;
+        this.hp = hp;
+        this.armour = armour;
+        this.damageDealt = damageDealt;
+        this.speed = speed;
+        this.x = x;
+        this.y = y;
+        this.resourceCost = resourceCost;
+        this.hpLeft = hp;
+    }
+}
