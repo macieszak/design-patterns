a9db1ea7c6045082d8bac3d529a05464411dddff
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-18 16:39:28 +0200

    Memento - exercise.

---
+++ memento-exercise/src/main/java/Main.java
@@ -0,0 +1,29 @@
+import backup_system.OperatingSystem;
+import backup_system.OperatingSystemCaretaker;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        OperatingSystemCaretaker operatingSystemCaretaker = new OperatingSystemCaretaker();
+        OperatingSystem operatingSystem = new OperatingSystem();
+
+        operatingSystem.createBackup();
+        Thread.sleep(3000);
+
+        operatingSystem.createBackup();
+        Thread.sleep(3000);
+
+        operatingSystemCaretaker.addMemento(operatingSystem.save());
+
+        operatingSystem.createBackup();
+        Thread.sleep(3000);
+
+        operatingSystem.createBackup();
+        Thread.sleep(3000);
+
+        operatingSystem.load(operatingSystemCaretaker.getMemento());
+
+    }
+
+}
---
+++ memento-exercise/src/main/java/backup_system/OperatingSystemCaretaker.java
@@ -0,0 +1,17 @@
+package backup_system;
+
+public class OperatingSystemCaretaker {
+
+    private OperatingSystemMemento operatingSystemMemento;
+
+    public void addMemento(OperatingSystemMemento operatingSystemMemento) {
+        System.out.println("Zapisano backup nr: " + operatingSystemMemento.getBackupNumber() + " z datą: " + operatingSystemMemento.getBackupDate());
+        this.operatingSystemMemento = operatingSystemMemento;
+    }
+
+    public OperatingSystemMemento getMemento() {
+        System.out.println("Wczytano backup nr: " + operatingSystemMemento.getBackupNumber() + " z datą: " + operatingSystemMemento.getBackupDate());
+        return operatingSystemMemento;
+    }
+
+}
