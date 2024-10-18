b8445cbf6059eea24d7e9c32104d067d73b3621f
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-18 13:24:58 +0200

    Initial status before implementing Command pattern.

---
+++ command/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import workshop.Robot;
+import workshop.WorkshopApp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        WorkshopApp workshopApp = new WorkshopApp(new Robot());
+        workshopApp.run();
+        
+    }
+
+
+}
---
+++ command/src/main/java/workshop/Robot.java
@@ -0,0 +1,21 @@
+package workshop;
+
+public class Robot {
+
+    public void turnOn() {
+        System.out.println("Robot został włączony!");
+    }
+
+    public void cut() {
+        System.out.println("Robot tnie!");
+    }
+
+    public void drill() {
+        System.out.println("Robot wierci!");
+    }
+
+    public void turnOff() {
+        System.out.println("Robot wyłączony!");
+    }
+
+}
