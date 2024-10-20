affa7becc36bcb02160f5fb866e7d161bf283614
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-20 15:52:38 +0200

    Initial status before implementing State pattern.

---
+++ state/src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        CoffeeMachine coffeeMachine = new CoffeeMachine();
+        coffeeMachine.insertTheCoin();
+        coffeeMachine.pushTheButton();
+        coffeeMachine.takeTheCup();
+
+    }
+
+}
