2d95a6cc4e2a6a65b3cced4b8e91baf4949a5e61
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-20 16:03:35 +0200

    Implementing the State pattern.

---
+++ state/src/main/java/State.java
@@ -0,0 +1,8 @@
+public interface State {
+
+    void insertTheCoin(CoffeeMachine coffeeMachine);
+    void pushTheButton(CoffeeMachine coffeeMachine);
+    void takeTheCup(CoffeeMachine coffeeMachine);
+    void returnTheCoin(CoffeeMachine coffeeMachine);
+
+}
