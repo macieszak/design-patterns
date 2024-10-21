e8e3ceab0679dd409296b555e32076353b8c8324
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-21 12:00:35 +0200

    Implementation of the Facade pattern.

--- facade/src/main/java/Main.java
+++ facade/src/main/java/Main.java
@@ -1,20 +1,10 @@
-import deliverybox.DeliveryBox;
-import deliverybox.DeliveryBoxSystem;
+import deliverybox.DeliveryBoxFacade;
 
 public class Main {
 
     public static void main(String[] args) {
-        pickupPackage();
-    }
-
-    private static void pickupPackage() {
-        DeliveryBox deliveryBox = new DeliveryBox();
-        DeliveryBoxSystem deliveryBoxSystem = new DeliveryBoxSystem();
-
-        deliveryBox.getUserData();
-        if (deliveryBoxSystem.isUserDataValidated() && deliveryBoxSystem.isPaymentSecured()) {
-            deliveryBox.openBox();
-        }
+        DeliveryBoxFacade deliveryBoxFacade = new DeliveryBoxFacade();
+        deliveryBoxFacade.pickupPackage();
     }
 
 }
---
+++ facade/src/main/java/deliverybox/DeliveryBoxFacade.java
@@ -0,0 +1,20 @@
+package deliverybox;
+
+public class DeliveryBoxFacade {
+
+    private DeliveryBox deliveryBox;
+    private DeliveryBoxSystem deliveryBoxSystem;
+
+    public DeliveryBoxFacade() {
+        this.deliveryBox = new DeliveryBox();
+        this.deliveryBoxSystem = new DeliveryBoxSystem();
+    }
+
+    public void pickupPackage() {
+        deliveryBox.getUserData();
+        if (deliveryBoxSystem.isUserDataValidated() && deliveryBoxSystem.isPaymentSecured()) {
+            deliveryBox.openBox();
+        }
+    }
+
+}
