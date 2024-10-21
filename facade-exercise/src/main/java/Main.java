90538c9ef2f5b1021582b47047fc2fa0350c27af
Maciej Maksymiuk <maksymiukmaciek@gmail.com>
2024-10-21 12:14:06 +0200

    Facade - exercise.

---
+++ facade-exercise/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import bank.AtmMachineFacade;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AtmMachineFacade atmMachineFacade = new AtmMachineFacade();
+        atmMachineFacade.withdrawMoney();
+
+    }
+
+}
